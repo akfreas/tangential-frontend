16cae1224efe705a6849ae4a838d98460786266a
Alexander Freas <akfreas@gmail.com>
2023-11-25 15:21:43 +0100

    Don't squish logo

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -10,7 +10,7 @@ import Image from 'next/image';
 const navigation = [
   { name: 'Planning', href: '/planning' },
   { name: 'Program View', href: '/' },
-  { name: 'Reports', href: '/reports' }
+  { name: 'Reports', href: '/reports' },
 ];
 
 function classNames(...classes: string[]) {
@@ -21,21 +21,21 @@ export default function Navbar({ user }: { user: any }) {
   const pathname = usePathname();
 
   return (
-    <Disclosure as="nav" className="bg-white shadow-sm">
+    <Disclosure as='nav' className='bg-white shadow-sm'>
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
-              <div className="flex">
-                <div className="flex flex-shrink-0 items-center">
+          <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+            <div className='flex h-16 justify-between'>
+              <div className='flex'>
+                <div className='flex flex-shrink-0 items-center'>
                   <Image
-                    src="/images/logo-no-background.png"
-                    alt="Tangential Logo"
+                    src='/images/logo-no-background.png'
+                    alt='Tangential Logo'
                     height={40}
-                    width={140}
+                    width={160}
                   />
                 </div>
-                <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                <div className='hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8'>
                   {navigation.map((item) => (
                     <a
                       key={item.name}
@@ -44,7 +44,7 @@ export default function Navbar({ user }: { user: any }) {
                         pathname === item.href
                           ? 'border-slate-500 text-gray-900'
                           : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium',
                       )}
                       aria-current={pathname === item.href ? 'page' : undefined}
                     >
@@ -53,13 +53,13 @@ export default function Navbar({ user }: { user: any }) {
                   ))}
                 </div>
               </div>
-              <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                <Menu as="div" className="relative ml-3">
+              <div className='hidden sm:ml-6 sm:flex sm:items-center'>
+                <Menu as='div' className='relative ml-3'>
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
-                      <span className="sr-only">Open user menu</span>
+                    <Menu.Button className='flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2'>
+                      <span className='sr-only'>Open user menu</span>
                       <Image
-                        className="h-8 w-8 rounded-full"
+                        className='h-8 w-8 rounded-full'
                         src={user?.image || 'https://avatar.vercel.sh/leerob'}
                         height={32}
                         width={32}
@@ -69,21 +69,21 @@ export default function Navbar({ user }: { user: any }) {
                   </div>
                   <Transition
                     as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
+                    enter='transition ease-out duration-200'
+                    enterFrom='transform opacity-0 scale-95'
+                    enterTo='transform opacity-100 scale-100'
+                    leave='transition ease-in duration-75'
+                    leaveFrom='transform opacity-100 scale-100'
+                    leaveTo='transform opacity-0 scale-95'
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
                       {user ? (
                         <Menu.Item>
                           {({ active }) => (
                             <button
                               className={classNames(
                                 active ? 'bg-gray-100' : '',
-                                'flex w-full px-4 py-2 text-sm text-gray-700'
+                                'flex w-full px-4 py-2 text-sm text-gray-700',
                               )}
                               onClick={() => signOut()}
                             >
@@ -97,7 +97,7 @@ export default function Navbar({ user }: { user: any }) {
                             <button
                               className={classNames(
                                 active ? 'bg-gray-100' : '',
-                                'flex w-full px-4 py-2 text-sm text-gray-700'
+                                'flex w-full px-4 py-2 text-sm text-gray-700',
                               )}
                               onClick={() => signIn('atlassian')}
                             >
@@ -110,31 +110,31 @@ export default function Navbar({ user }: { user: any }) {
                   </Transition>
                 </Menu>
               </div>
-              <div className="-mr-2 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
-                  <span className="sr-only">Open main menu</span>
+              <div className='-mr-2 flex items-center sm:hidden'>
+                <Disclosure.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2'>
+                  <span className='sr-only'>Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
                   )}
                 </Disclosure.Button>
               </div>
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 pt-2 pb-3">
+          <Disclosure.Panel className='sm:hidden'>
+            <div className='space-y-1 pt-2 pb-3'>
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
-                  as="a"
+                  as='a'
                   href={item.href}
                   className={classNames(
                     pathname === item.href
                       ? 'bg-slate-50 border-slate-500 text-slate-700'
                       : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
-                    'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                    'block pl-3 pr-4 py-2 border-l-4 text-base font-medium',
                   )}
                   aria-current={pathname === item.href ? 'page' : undefined}
                 >
@@ -142,42 +142,42 @@ export default function Navbar({ user }: { user: any }) {
                 </Disclosure.Button>
               ))}
             </div>
-            <div className="border-t border-gray-200 pt-4 pb-3">
+            <div className='border-t border-gray-200 pt-4 pb-3'>
               {user ? (
                 <>
-                  <div className="flex items-center px-4">
-                    <div className="flex-shrink-0">
+                  <div className='flex items-center px-4'>
+                    <div className='flex-shrink-0'>
                       <Image
-                        className="h-8 w-8 rounded-full"
+                        className='h-8 w-8 rounded-full'
                         src={user.image}
                         height={32}
                         width={32}
                         alt={`${user.name} avatar`}
                       />
                     </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium text-gray-800">
+                    <div className='ml-3'>
+                      <div className='text-base font-medium text-gray-800'>
                         {user.name}
                       </div>
-                      <div className="text-sm font-medium text-gray-500">
+                      <div className='text-sm font-medium text-gray-500'>
                         {user.email}
                       </div>
                     </div>
                   </div>
-                  <div className="mt-3 space-y-1">
+                  <div className='mt-3 space-y-1'>
                     <button
                       onClick={() => signOut()}
-                      className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                      className='block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800'
                     >
                       Sign out
                     </button>
                   </div>
                 </>
               ) : (
-                <div className="mt-3 space-y-1">
+                <div className='mt-3 space-y-1'>
                   <button
                     onClick={() => signIn('atlassian')}
-                    className="flex w-full px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800"
+                    className='flex w-full px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800'
                   >
                     Sign in
                   </button>
